6f76f8bd2dcbc9dd218b2a1fb2a4c192bd3c7718
medecom34-ui <medecom34@gmail.com>
2026-01-29 12:30:58 +0700

    fix payment ui

--- server/src/index.js
+++ server/src/index.js
@@ -11,8 +11,8 @@ const app = express();
 
 app.use(cors({
   origin: [
-    "https://medecom34-ui.github.io",
-    "https://www.xn--12ca7coduykzdcc8j5bxa4g1h0hh.com"
+    "https://www.xn--12ca7coduykzdcc8j5bxa4g1h0hh.com",
+    "https://medecom34-ui.github.io"
   ],
   methods: ["GET", "POST", "PATCH", "DELETE"],
   credentials: true
